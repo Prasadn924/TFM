30ac8dd42a1ede3b1c3f9ac31b03622c02f52bd4
Prasadn924 <74699858+Prasadn924@users.noreply.github.com>
2021-03-01 10:17:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
   access_key = "AKIA5PV5S5YNILK5OJXV"
   secret_key = "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"
 }
 resource "aws_instance" "Myfirstinstance" {
-  ami = "ami-0915bcb5fa77e4892"
+  ami = "ami-09246ddb00c7c4fef"
   instance_type = "t2.micro"
 } 
